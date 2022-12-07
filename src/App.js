4517adda15469a02274b629b53653ac968ad3a76
Prajwal Magar <pzlmagar@gmail.com>
2022-12-07 12:20:16 +0545

    Commit 1
    First commit of the page.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Analytics from './Components/Analytics';
+import Cards from './Components/Cards';
+import Footer from './Components/Footer';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+import Newsletter from './Components/Newsletter';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics/>
+      <Newsletter/>
+      <Cards/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
